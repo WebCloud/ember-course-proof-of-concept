73527302bcf09bfee837c39b4ea6f94a4f244586
Vinicius Dallacqua <vinicius.dallacqua@netent.com>
2016-10-07 01:03:05 +0200

    step 7

--- app/services/authentication.js
+++ app/services/authentication.js
@@ -6,17 +6,16 @@ export default Ember.Service.extend({
   currentUser: null,
   isAuthenticated: Ember.computed.bool('currentUser'),
 
+  init() {
+    this._refreshSession();
+  },
+
   login(user) {
     return new Promise((resolve, reject) => {
       if (Ember.isNone(user)) {
         reject('A user object must be provided');
       } else {
-        this.get('store').queryRecord('user', user)
-          .then((user) => {
-            this.set('currentUser', user);
-
-            return user;
-          })
+        this._fetchCurrentUser(user)
           .then(this._saveSession)
           .then(resolve)
           .catch(reject);
@@ -24,10 +23,34 @@ export default Ember.Service.extend({
     });
   },
 
+  _fetchCurrentUser(user) {
+    return this.get('store').queryRecord('user', user)
+      .then((user) => {
+        this.set('currentUser', user);
+
+        return user;
+      });
+  },
+
   _saveSession(userInstance) {
     const currentUser = userInstance.getProperties('email', 'name');
     sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
 
     return userInstance;
+  },
+
+  _refreshSession() {
+    return new Promise((resolve, reject) => {
+      const userSession = sessionStorage.getItem('currentUser')
+
+      if (Ember.isPresent(userSession)) {
+        const userObject = JSON.parse(userSession);
+        this._fetchCurrentUser(userObject)
+          .then(resolve)
+          .catch(reject);
+      } else {
+        reject('No user session found');
+      }
+    });
   }
 });
