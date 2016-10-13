5f91051f2addd135ed2cb24e72dd173f1a11ef4d
Vinicius Dallacqua <vinicius.dallacqua@netent.com>
2016-10-14 00:01:45 +0200

    stage 12

--- app/routes/authenticated.js
+++ app/routes/authenticated.js
@@ -14,5 +14,14 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     this._super(controller, model);
     controller.set('currentUser', this.get('authentication.currentUser'));
+  },
+
+  actions: {
+    deleteUser() {
+      this.get('authentication.currentUser')
+        .destroyRecord()
+        .then(() => this.get('authentication').logout())
+        .then(() => this.transitionTo('index'));
+    }
   }
 });
