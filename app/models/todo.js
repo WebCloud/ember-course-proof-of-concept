76e198b550666f719579b5eb137b90c97a1c8512
Vinicius Dallacqua <vinicius.dallacqua@netent.com>
2016-11-16 10:14:20 +0100

    step 18

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr(),
   isDone: DS.attr('boolean'),
   user: DS.belongsTo('user'),
-  isEditing: false
+  isEditing: false,
+  isDoneObserver: Ember.observer('isDone', function() {
+    this.save();
+  }),
+  isInvalid: Ember.computed.empty('title')
 });
